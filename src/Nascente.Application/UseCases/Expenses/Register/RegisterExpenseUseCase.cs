5cc7155cbe2e77efb895b72885f87976ef11b125
EvilisGlenio <glenio.developer@gmail.com>
2025-04-22 23:25:50 -0300

    feat(register): add FluentValidation and map validation errors using LINQ

--- src/Nascente.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/Nascente.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -10,39 +10,20 @@ public class RegisterExpenseUseCase
     {
         Validate(request);
 
-        return new ResponseRegisterExpenseJson
-        {
-            Title = request.Title,
-            Description = request.Description,
-            Amount = request.Amount,
-            PaymentType = request.PaymentType,
-            Date = request.Date
-        };
+        return new ResponseRegisterExpenseJson();
     }
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if (titleIsEmpty)
+        var validator = new RegisterExpenseValidator();
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
         {
-            throw new ArgumentException("Title cannot be empty!");
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ArgumentException();
         }
 
-        if (request.Amount <= 0)
-        {
-            throw new ArgumentException("Amount must be greater than zero!");
-        }
-
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-        if (!paymentTypeIsValid)
-        {
-            throw new ArgumentException("Invalid payment type");
-        }
-
-        var dateTime = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if (dateTime <= 0)
-        {
-            throw new ArgumentException("Date cannot be in the future!");
-        }
     }
 }
---
+++ src/Nascente.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Nascente.Communication.Requests;
+
+namespace Nascente.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("Title is required!");
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("Amount must be greater than zero!");
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Date cannot be in the future!");
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Invalid payment type!");
+    }
+}
