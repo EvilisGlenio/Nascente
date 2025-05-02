97ca1ebbbdf71db9b8cd1785f80d8d8d9d24ffe3
EvilisGlenio <glenio.developer@gmail.com>
2025-05-02 09:31:22 -0300

    feat: validate client language against supported cultures in middleware

--- src/Nascente.Api/Program.cs
+++ src/Nascente.Api/Program.cs
@@ -1,4 +1,5 @@
 using Nascente.Api.Filters;
+using Nascente.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
