941f22cb6f22a46b27a5f621191ae3ca0c0f9daf
EvilisGlenio <glenio.developer@gmail.com>
2025-05-03 11:12:46 -0300

    test: add unit tests for Validator class

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using Nascente.Application.UseCases.Expenses.Register;
+using Nascente.Communication.Requests;
+
+namespace Validators.Tests.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Title = "Title",
+            Description = "Description",
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            PaymentType = Nascente.Communication.Enums.PaymentType.Cash
+
+        };
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+
+    }
+}
