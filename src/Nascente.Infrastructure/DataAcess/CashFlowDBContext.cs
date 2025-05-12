e2b4ce110d4a573ba125c54c82f69d8c263deed5
EvilisGlenio <glenio.developer@gmail.com>
2025-05-12 10:05:44 -0300

    feat(infrastructure): add database context

---
+++ src/Nascente.Infrastructure/DataAcess/CashFlowDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nascente.Domain.Entities;
+
+namespace Nascente.Infrastructure.DataAcess;
+
+public class CashFlowDBContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Password123;";
+        var version = new Version(8, 0, 41);
+        var serverVersion = new MySqlServerVersion(version);
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
